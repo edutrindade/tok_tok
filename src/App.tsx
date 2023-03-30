fb3986c36dc5c56c723aa1dd906b9bcaf813c6c8
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2023-03-30 00:07:36 -0300

    Componente de vídeo

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Video from './pages/Video';
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="videos">
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
+}
+
+export default App;
