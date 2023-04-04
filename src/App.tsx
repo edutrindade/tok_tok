311bb4ab9318ffb57edb54e415d73f7305125a9c
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2023-04-03 21:14:13 -0300

    Conexão com Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-
 import Video from './pages/Video';
 
+import db from './config/firebase';
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { VideoProps } from './interfaces/VideoProps';
+
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCol = collection(db, 'videos');
+    const videoSnapshot = await getDocs(videosCol);
+    const videosList = videoSnapshot.docs.map(doc => doc.data());
+
+    setVideos(videosList as any);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, [])
 
   return (
     <div className="app">
       <div className="videos">
-        <Video
+        {videos.map((video: VideoProps, index) => {
+          return (
+            <Video
+              key={String(index)}
+              likes={video.likes}
+              messages={video.messages}
+              shares={video.shares}
+              name={video.name}
+              description={video.description}
+              music={video.music}
+              url={video.url}
+            />
+          )
+        })}
+        {/* <Video
           likes={300}
           messages={200}
           shares={100}
@@ -24,7 +55,7 @@ function App() {
           description="Descrição do Vídeo"
           music="Título da Música"
           url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
-        />
+        /> */}
       </div>
     </div>
   );
